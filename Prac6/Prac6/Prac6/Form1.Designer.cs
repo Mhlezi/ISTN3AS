00e3be21dfaf37e4f6f75c039de1a1270c2e2218
Mhlezi <mhlezi03@gmail.com>
2026-05-22 03:04:27 +0200

    Add files via upload
    keep

---
+++ Prac6/Prac6/Prac6/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Prac6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvBookList = new System.Windows.Forms.DataGridView();
+            this.dsPrac6 = new Prac6.DataSet1();
+            this.taBookOrder = new Prac6.DataSet1TableAdapters.BookOrderTableAdapter();
+            this.taBook = new Prac6.DataSet1TableAdapters.BookTableAdapter();
+            this.bookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTitleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookCostDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qOHDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnUpdateStock = new System.Windows.Forms.Button();
+            this.txtUpdateStock = new System.Windows.Forms.TextBox();
+            this.dgvInvoice = new System.Windows.Forms.DataGridView();
+            this.BookTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookCost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvBookOrder = new System.Windows.Forms.DataGridView();
+            this.OrderNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OrderDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OrderAmt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsPrac6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1403, 697);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnRecord);
+            this.tabPage1.Controls.Add(this.txtTotal);
+            this.tabPage1.Controls.Add(this.dgvInvoice);
+            this.tabPage1.Controls.Add(this.txtUpdateStock);
+            this.tabPage1.Controls.Add(this.btnUpdateStock);
+            this.tabPage1.Controls.Add(this.txtSearch);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1395, 671);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Book";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvBookOrder);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1395, 671);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Book Order";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvBookList);
+            this.groupBox1.Location = new System.Drawing.Point(56, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Book List";
+            // 
+            // dgvBookList
+            // 
+            this.dgvBookList.AutoGenerateColumns = false;
+            this.dgvBookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bookTitleDataGridViewTextBoxColumn,
+            this.bookCostDataGridViewTextBoxColumn,
+            this.qOHDataGridViewTextBoxColumn});
+            this.dgvBookList.DataSource = this.bookBindingSource;
+            this.dgvBookList.Location = new System.Drawing.Point(16, 30);
+            this.dgvBookList.Name = "dgvBookList";
+            this.dgvBookList.Size = new System.Drawing.Size(437, 200);
+            this.dgvBookList.TabIndex = 0;
+            this.dgvBookList.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBookList_CellContentDoubleClick);
+            // 
+            // dsPrac6
+            // 
+            this.dsPrac6.DataSetName = "dsPrac6";
+            this.dsPrac6.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // taBookOrder
+            // 
+            this.taBookOrder.ClearBeforeFill = true;
+            // 
+            // taBook
+            // 
+            this.taBook.ClearBeforeFill = true;
+            // 
+            // bookBindingSource
+            // 
+            this.bookBindingSource.DataMember = "Book";
+            this.bookBindingSource.DataSource = this.dsPrac6;
+            // 
+            // bookTitleDataGridViewTextBoxColumn
+            // 
+            this.bookTitleDataGridViewTextBoxColumn.DataPropertyName = "BookTitle";
+            this.bookTitleDataGridViewTextBoxColumn.HeaderText = "BookTitle";
+            this.bookTitleDataGridViewTextBoxColumn.Name = "bookTitleDataGridViewTextBoxColumn";
+            // 
+            // bookCostDataGridViewTextBoxColumn
+            // 
+            this.bookCostDataGridViewTextBoxColumn.DataPropertyName = "BookCost";
+            this.bookCostDataGridViewTextBoxColumn.HeaderText = "BookCost";
+            this.bookCostDataGridViewTextBoxColumn.Name = "bookCostDataGridViewTextBoxColumn";
+            // 
+            // qOHDataGridViewTextBoxColumn
+            // 
+            this.qOHDataGridViewTextBoxColumn.DataPropertyName = "QOH";
+            this.qOHDataGridViewTextBoxColumn.HeaderText = "QOH";
+            this.qOHDataGridViewTextBoxColumn.Name = "qOHDataGridViewTextBoxColumn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Book Name";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(260, 49);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(209, 20);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnUpdateStock
+            // 
+            this.btnUpdateStock.Location = new System.Drawing.Point(191, 358);
+            this.btnUpdateStock.Name = "btnUpdateStock";
+            this.btnUpdateStock.Size = new System.Drawing.Size(141, 45);
+            this.btnUpdateStock.TabIndex = 4;
+            this.btnUpdateStock.Text = "Update Stock";
+            this.btnUpdateStock.UseVisualStyleBackColor = true;
+            this.btnUpdateStock.Click += new System.EventHandler(this.btnUpdateStock_Click);
+            // 
+            // txtUpdateStock
+            // 
+            this.txtUpdateStock.Location = new System.Drawing.Point(361, 377);
+            this.txtUpdateStock.Name = "txtUpdateStock";
+            this.txtUpdateStock.Size = new System.Drawing.Size(173, 20);
+            this.txtUpdateStock.TabIndex = 5;
+            // 
+            // dgvInvoice
+            // 
+            this.dgvInvoice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInvoice.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BookTitle,
+            this.BookCost});
+            this.dgvInvoice.Location = new System.Drawing.Point(692, 132);
+            this.dgvInvoice.Name = "dgvInvoice";
+            this.dgvInvoice.Size = new System.Drawing.Size(408, 150);
+            this.dgvInvoice.TabIndex = 6;
+            // 
+            // BookTitle
+            // 
+            this.BookTitle.HeaderText = "BookTitle";
+            this.BookTitle.Name = "BookTitle";
+            // 
+            // BookCost
+            // 
+            this.BookCost.HeaderText = "BookCost";
+            this.BookCost.Name = "BookCost";
+            // 
+            // dgvBookOrder
+            // 
+            this.dgvBookOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookOrder.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OrderNo,
+            this.OrderDate,
+            this.OrderAmt});
+            this.dgvBookOrder.Location = new System.Drawing.Point(215, 112);
+            this.dgvBookOrder.Name = "dgvBookOrder";
+            this.dgvBookOrder.Size = new System.Drawing.Size(364, 150);
+            this.dgvBookOrder.TabIndex = 0;
+            // 
+            // OrderNo
+            // 
+            this.OrderNo.HeaderText = "OrderNo";
+            this.OrderNo.Name = "OrderNo";
+            // 
+            // OrderDate
+            // 
+            this.OrderDate.HeaderText = "OrderDate";
+            this.OrderDate.Name = "OrderDate";
+            // 
+            // OrderAmt
+            // 
+            this.OrderAmt.HeaderText = "OrderAmt";
+            this.OrderAmt.Name = "OrderAmt";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(965, 305);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(135, 20);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(692, 305);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(181, 47);
+            this.btnRecord.TabIndex = 8;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1403, 697);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsPrac6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookOrder)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvBookList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookTitleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookCostDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qOHDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource bookBindingSource;
+        private DataSet1 dsPrac6;
+        private DataSet1TableAdapters.BookOrderTableAdapter taBookOrder;
+        private DataSet1TableAdapters.BookTableAdapter taBook;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUpdateStock;
+        private System.Windows.Forms.Button btnUpdateStock;
+        private System.Windows.Forms.DataGridView dgvInvoice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookCost;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.DataGridView dgvBookOrder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderAmt;
+        private System.Windows.Forms.Button btnRecord;
+    }
+}
+
---
+++ Prac6/Prac6/Prac6/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prac6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.taBook.FillByLoad(this.dsPrac6.Book);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            taBook.FillBySearch(dsPrac6.Book, txtSearch.Text);
+        }
+
+        private void btnUpdateStock_Click(object sender, EventArgs e)
+        {
+            taBook.UpdateQuery(int.Parse(txtUpdateStock.Text), dgvBookList.CurrentRow.Cells[0].Value.ToString());
+            taBook.FillByLoad(dsPrac6.Book);
+        }
+
+        private void dgvBookList_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvInvoice.Rows.Add(
+                dgvBookList.CurrentRow.Cells[0].Value.ToString(), 
+                dgvBookList.CurrentRow.Cells[1].Value.ToString()
+                );
+
+            decimal total = 0;
+            foreach (DataGridViewRow row in dgvInvoice.Rows)
+            {
+                if (row.Cells[1].Value != null)
+                {
+                    total += decimal.Parse(row.Cells[1].Value.ToString());
+                }
+            }
+            txtTotal.Text = total.ToString("c2");
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            int pk;
+            DialogResult result = MessageBox.Show("Do you want to Confirm?", "Confirmation", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.Yes)
+            {
+                pk = (int)taBookOrder.InsertQueryPK(DateTime.Now, Decimal.Parse(txtTotal.Text, NumberStyles.Currency));
+                MessageBox.Show("The Order for a Total Amount of " + txtTotal.Text + "has been Confirmed and the PK is " + pk);
+                taBookOrder.Fill(dsPrac6.BookOrder); //to fill the gridview with the newly recorded order so that it can be viewed
+                for (int i = 0; i < dgvInvoice.Rows.Count - 1; i++) //record the inner detail of the Order
+                {
+                    string bkID = dgvInvoice.Rows[i].Cells[0].Value.ToString();
+                    decimal bkPrice = (decimal)dgvInvoice.Rows[i].Cells[1].Value;
+                    taBookOrder.Insert(pk, bkID, 1, bkPrice);
+                }
+            }
+
+        }
+    }
+}
